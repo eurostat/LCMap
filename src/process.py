be64a5b41dd376192f8466a798d3f3772e47bdf5
jgaffuri <jgaf@proton.me>
2025-09-15 18:18:11 +0200

    a

--- src/process.py
+++ src/process.py
@@ -8,50 +8,54 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from utils.geotiff import resample_geotiff_aligned
 
-inpath = "/home/juju/geodata/CLC/"
-resolutions = [10000, 5000, 2000, 1000, 500, 200, 100]
+
+
+resolutions = [10000, 5000, 2000, 1000, 500] #, 200, 100]
+resampling = False
+tiling = True
+
 
 
 # resampling
-input_files = {
-    "2018": "u2018_clc2018_v2020_20u1_raster100m/DATA/U2018_CLC2018_V2020_20u1.tif",
-    "2012": "u2018_clc2012_v2020_20u1_raster100m/DATA/U2018_CLC2012_V2020_20u1.tif",
-    "2006": "u2012_clc2006_v2020_20u1_raster100m/DATA/U2012_CLC2006_V2020_20u1.tif",
-    "2000": "u2006_clc2000_v2020_20u1_raster100m/DATA/U2006_CLC2000_V2020_20u1.tif",
-    "1990": "u2000_clc1990_v2020_20u1_raster100m/DATA/U2000_CLC1990_V2020_20u1.tif",
-}
-for resolution in resolutions:
-    for year in ["2018", "2012", "2006", "2000", "1990"]:
-        print(datetime.now(), "resampling", year, resolution)
-        infile = input_files[year]
-        outfile = "./tmp/"+year+"_"+str(resolution)+".tif"
-        resample_geotiff_aligned(inpath + infile, outfile, resolution, resampling=Resampling.mode, dtype=np.int8)
-
-
-'''
-# tiling
-for year in ["2018", "2012", "2006", "2000", "1990"]:
-
+if resampling:
+    inpath = "/home/juju/geodata/CLC/"
+    input_files = {
+        "2018": "u2018_clc2018_v2020_20u1_raster100m/DATA/U2018_CLC2018_V2020_20u1.tif",
+        "2012": "u2018_clc2012_v2020_20u1_raster100m/DATA/U2018_CLC2012_V2020_20u1.tif",
+        "2006": "u2012_clc2006_v2020_20u1_raster100m/DATA/U2012_CLC2006_V2020_20u1.tif",
+        "2000": "u2006_clc2000_v2020_20u1_raster100m/DATA/U2006_CLC2000_V2020_20u1.tif",
+        "1990": "u2000_clc1990_v2020_20u1_raster100m/DATA/U2000_CLC1990_V2020_20u1.tif",
+    }
     for resolution in resolutions:
-        print(datetime.now(), "Tiling", year, resolution)
+        for year in ["2018", "2012", "2006", "2000", "1990"]:
+            print(datetime.now(), "resampling", year, resolution)
+            infile = input_files[year]
+            outfile = "./tmp/"+year+"_"+str(resolution)+".tif"
+            resample_geotiff_aligned(inpath + infile, outfile, resolution, resampling=Resampling.mode, dtype=np.int8)
 
-        # make folder for resolution
-        folder_ = "./out/v1/"+year+"/"+str(resolution)+"/"
-        if not os.path.exists(folder_): os.makedirs(folder_)
 
-        # prepare dict for geotiff bands
-        dict = {}
-        dict["code"] = { "file" : "./tmp/"+year+"_"+str(resolution)+".tif", "band":1 }
+if tiling:
+    # tiling
+    for year in ["2018", "2012", "2006", "2000", "1990"]:
 
-        # launch tiling
-        gridtiler_raster.tiling_raster(
-            dict,
-            folder_,
-            crs="EPSG:3035",
-            tile_size_cell = 512,
-            format="parquet",
-            num_processors_to_use = 10,
-            #modif_fun = lambda v: int(v),
-            )
-'''
+        for resolution in resolutions:
+            print(datetime.now(), "Tiling", year, resolution)
 
+            # make folder for resolution
+            folder_ = "./out/v1/"+year+"/"+str(resolution)+"/"
+            if not os.path.exists(folder_): os.makedirs(folder_)
+
+            # prepare dict for geotiff bands
+            dict = {}
+            dict["code"] = { "file" : "./tmp/"+year+"_"+str(resolution)+".tif", "band":1 }
+
+            # launch tiling
+            gridtiler_raster.tiling_raster(
+                dict,
+                folder_,
+                crs="EPSG:3035",
+                tile_size_cell = 512,
+                format="parquet",
+                num_processors_to_use = 10,
+                #modif_fun = lambda v: int(v),
+                )
