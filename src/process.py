2345ed6c209b0d48c3ffe2fe3296467562de8b1a
jgaffuri <jgaf@proton.me>
2025-09-16 08:47:34 +0200

    a

--- src/process.py
+++ src/process.py
@@ -10,8 +10,8 @@ from utils.geotiff import resample_geotiff_aligned
 
 
 
-resolutions = [200, 100] #[10000, 5000, 2000, 1000, 500] #, 200, 100]
-years = ["2012", "2006", "2000", "1990"]
+resolutions = [10000, 5000, 2000, 1000, 500] #, 200, 100]
+years = ["2018", "2012", "2006", "2000", "1990"]
 resampling = False
 tiling = False
 tiling_change = True
